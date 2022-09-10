5c50744b90b4a74fdded9996a82468011ec3bc13
janWICKEDpro <janroyal45@gmail.com>
2022-09-10 07:04:58 +0100

    more starter files included

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:tik_tok_clone/screens/mainfeed/mainfeed.dart';
+import 'package:tik_tok_clone/screens/profile/profile.dart';
+import 'package:tik_tok_clone/screens/search/search.dart';
+
+void main() {
+  runApp(const TikTokApp());
+}
+
+class TikTokApp extends StatelessWidget {
+  const TikTokApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        themeMode: ThemeMode.dark,
+        darkTheme: ThemeData.dark(),
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          visualDensity: const VisualDensity(horizontal: 2.0, vertical: 2.0),
+          primaryColorLight: const Color(0xff03203C),
+          primaryColorDark: const Color(0xff242B2E),
+
+          // Icon Theme
+          iconTheme:
+              const IconThemeData(color: Colors.amber, size: 15.0, opacity: 10),
+        ),
+        routes: {
+          '/': (context) => MainFeed(),
+          '/profile': (context) => Profile(),
+          '/search': (context) => Search()
+        });
+  }
+}
---
+++ lib/screens/mainfeed/mainfeed.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MainFeed extends StatelessWidget {
+  const MainFeed({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(child: Center(child: Text("hi"))),
+    );
+  }
+}
