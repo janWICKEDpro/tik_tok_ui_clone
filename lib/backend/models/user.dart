7d72d915be804352957ae4f86c4d9506ba114904
janWICKEDpro <janroyal45@gmail.com>
2022-10-12 15:56:59 +0100

    user class updated

--- lib/backend/models/user.dart
+++ lib/backend/models/user.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
+
 class User {
+  String? uid;
   String? username;
   String? email;
   String? password;
@@ -7,13 +10,36 @@ class User {
   int? numberOfPosts;
 
   User(
-      {this.email,
+      {@required this.uid,
+      this.email,
+      this.password,
       this.followers,
       this.following,
       this.numberOfPosts,
       this.username});
 
   String toJson() {
-    return "{username: $username, email: $email, password:$password }";
+    return "{'username': $username, 'email': $email, 'password': $password, 'id': $uid, 'followers': $followers, 'following': $following, 'numberOfPosts': $numberOfPosts }";
+  }
+
+  factory User.fromJson(Map<String, Object>? user) {
+    if (user == null) {
+      return User();
+    }
+    final id = user['id'] as String;
+    final email = user['email'] as String;
+    final followers = user['followers'] as int;
+    final following = user['following'] as int;
+    final posts = user['posts'] as int;
+    final username = user['username'] as String;
+    final password = user['password'] as String;
+    return User(
+        uid: id,
+        email: email,
+        username: username,
+        followers: followers,
+        following: following,
+        numberOfPosts: posts,
+        password: password);
   }
 }
