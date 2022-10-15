b28c63876986b293736683b855bb0388febbef33
janWICKEDpro <janroyal45@gmail.com>
2022-10-15 17:27:28 +0100

    classes shaping up

--- lib/backend/models/comment.dart
+++ lib/backend/models/comment.dart
@@ -2,4 +2,5 @@ class Comment {
   Comment({this.uid, this.comment});
   String? uid;
   String? comment;
+  Map<String, String>? replies;
 }
--- lib/backend/models/video_post.dart
+++ lib/backend/models/video_post.dart
@@ -1,9 +1,19 @@
+import 'package:tik_tok_clone/backend/models/comment.dart';
+
 class Post {
-  Post({this.id, this.uid, this.title});
+  Post(
+      {this.id,
+      this.uid,
+      this.title,
+      this.comment,
+      this.likes,
+      this.link,
+      this.views});
   String? id;
   String? uid;
   String? title;
   String? link;
   int? views;
   int? likes;
+  Comment? comment;
 }
