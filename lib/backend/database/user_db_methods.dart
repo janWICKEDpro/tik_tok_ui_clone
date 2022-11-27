2377783bf21dcda5472753a3b77a12e6a3bb8f00
janWICKEDpro <janroyal45@gmail.com>
2022-11-27 22:39:48 +0100

    cleanup

--- lib/backend/database/user_db_methods.dart
+++ lib/backend/database/user_db_methods.dart
@@ -3,58 +3,58 @@ import 'dart:convert' as convert;
 
 import '../models/user.dart';
 
-// class UserDatabaseMethods {
-//   final FirebaseFirestore _db = FirebaseFirestore.instance;
+class UserDatabaseMethods {
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
 
-//   Future<bool> addUser(User? user) async {
-//     if (user == null) {
-//       return false;
-//     }
-//     try {
-//       await _db
-//           .collection('USERS')
-//           .doc(user.uid)
-//           .set(convert.jsonDecode(user.toJson()) as Map<String, dynamic>);
-//       return true;
-//     } catch (e) {
-//       return false;
-//     }
-//   }
+  Future<bool> addUser(User? user) async {
+    if (user == null) {
+      return false;
+    }
+    try {
+      await _db
+          .collection('USERS')
+          .doc(user.uid)
+          .set(convert.jsonDecode(user.toJson()) as Map<String, dynamic>);
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
 
-//   Future<bool> deleteUser(User? user) async {
+  Future<bool> deleteUser(User? user) async {
   
-//     if (user == null) {
-//       return false;
-//     }
-//     try {
-//      await _db.collection('USERS').doc('${user.uid}').delete();
-//       return true;
-//     } catch (e) {
-//       print(e);
-//       return false;
-//     }
-//   }
+    if (user == null) {
+      return false;
+    }
+    try {
+     await _db.collection('USERS').doc('${user.uid}').delete();
+      return true;
+    } catch (e) {
+      print(e);
+      return false;
+    }
+  }
 
-//   Future<bool> updateField(
-//     String field,
-//   ) async {
-//     //todo: to be  implemented;
-//   }
+  Future<bool> updateField(
+    String field,
+  ) async {
+    //todo: to be  implemented;
+  }
 
-//   Future<bool> updateFollowing(User? user) async {
-//     //todo: to be implemented;
-//     try {
-//      await  _db.collection('USERS').doc('${user!.uid}').update(data)
-//     } catch (e) {
-//       print(e);
-//       return false;
-//     }
-//   }
+  Future<bool> updateFollowing(User? user) async {
+    //todo: to be implemented;
+    try {
+     await  _db.collection('USERS').doc('${user!.uid}').update(data)
+    } catch (e) {
+      print(e);
+      return false;
+    }
+  }
 
-//   Future<User> getUser() async {
-//     //todo: to be implemented;
-//   }
-//   Future getUserPosts() async {
-//     //todo: to be implememted;
-//   }
-// }
+  Future<User> getUser() async {
+    //todo: to be implemented;
+  }
+  Future getUserPosts() async {
+    //todo: to be implememted;
+  }
+}
